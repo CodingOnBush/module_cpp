03a38dd5257a609bbdde229c6937f71db971d0f1
Mostafa Omrane <momrane@made-f0Dr7s7.clusters.42paris.fr>
2024-06-03 18:36:47 +0200

    cpp04 ex03 done but need to be checked

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:59:24 by momrane           #+#    #+#             */
-/*   Updated: 2024/06/03 15:05:00 by momrane          ###   ########.fr       */
+/*   Updated: 2024/06/03 18:36:05 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,82 @@ int	main(void)
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 	
-	ICharacter* me = new Character("me");
+	ICharacter*	me = new Character("me");
+	AMateria* 	materia1;
+	AMateria* 	materia2;
+	AMateria* 	materia3;
+	AMateria* 	materia4;
 	
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+	materia1 = src->createMateria("ice");
+	me->equip(materia1);
+
+	materia2 = src->createMateria("cure");
+	me->equip(materia2);
+
+	materia3 = src->createMateria("ice");
+	me->equip(materia3);
+
+	materia4 = src->createMateria("cure");
+	me->equip(materia4);
+
+	std::cout << "1-------------------------" << std::endl;
+	me->use(0, *me);
+	me->use(1, *me);
+	me->use(2, *me);
+	me->use(3, *me);
+	std::cout << "-------------------------" << std::endl;
+
+	me->unequip(3);
+
+	std::cout << "2-------------------------" << std::endl;
+	me->use(3, *me);
+	std::cout << "-------------------------" << std::endl;
+
+	me->equip(materia4);
+	
+	std::cout << "3-------------------------" << std::endl;
+	me->use(3, *me);
+	std::cout << "-------------------------" << std::endl;
+
 	
 	ICharacter* bob = new Character("bob");
 	
+	std::cout << "4-------------------------" << std::endl;
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
+	std::cout << "-------------------------" << std::endl;
 	
 	delete bob;
 	delete me;
 	delete src;
 	
-	return 0;
+	return (0);
 }
+
+// int	main(void)
+// {
+// 	IMateriaSource* src = new MateriaSource();
+// 	src->learnMateria(new Ice());
+// 	src->learnMateria(new Cure());
+	
+// 	ICharacter* me = new Character("me");
+	
+// 	AMateria* tmp;
+// 	tmp = src->createMateria("ice");
+// 	me->equip(tmp);
+// 	tmp = src->createMateria("cure");
+// 	me->equip(tmp);
+	
+// 	ICharacter* bob = new Character("bob");
+	
+// 	me->use(0, *bob);
+// 	me->use(1, *bob);
+	
+// 	delete bob;
+// 	delete me;
+// 	delete src;
+	
+// 	return (0);
+// }
